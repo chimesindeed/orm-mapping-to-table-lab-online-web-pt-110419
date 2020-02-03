7f4c21558dcdb48a2dedf2b75a2908c0b2cd1fc4
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-02-03 02:08:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,6 +31,9 @@ def save
   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   
 end
+
+def self.create(name, grade)
+  name = Student.new(name, grade)
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
